fe6a24612f0c9500eb859eff1ab68dd14b249f55
Kongpob-R <kongpop.rak@hotmail.co.th>
2022-01-21 15:42:26 +0700

    add more details for vendor

--- models/Vendor.js
+++ models/Vendor.js
@@ -4,6 +4,10 @@ const vendor = new mongoose.Schema({
 	name: String,
 	billerId: String,
 	address: String,
+	subDistrict: String,
+	district: String,
+	province: String,
+	zipcode: String,
 });
 
 var Vendor = mongoose.model("Vendor", vendor);
